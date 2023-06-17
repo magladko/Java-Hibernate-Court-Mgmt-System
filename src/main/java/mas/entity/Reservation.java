c2e446304c745f4f9086c39c9303125aa968a9a3
magladko <kornel.marek@gmail.com>
2023-06-18 00:16:36 +0200

    minor import cleanup

--- src/main/java/mas/entity/Reservation.java
+++ src/main/java/mas/entity/Reservation.java
@@ -10,7 +10,6 @@ import org.hibernate.TypeMismatchException;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.Optional;
 
 @Entity
