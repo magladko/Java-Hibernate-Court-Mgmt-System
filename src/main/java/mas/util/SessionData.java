bd9c1601001be71ff3468961af984f715a39610f
magladko <kornel.marek@gmail.com>
2023-06-21 21:33:42 +0200

    SessionData docs

--- src/main/java/mas/util/SessionData.java
+++ src/main/java/mas/util/SessionData.java
@@ -12,6 +12,10 @@ import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+/**
+ * This class represents session data for the Tennis Courts management application.
+ * It stores information about court reservations, participants, prices, and comments.
+ */
 @NoArgsConstructor
 public class SessionData {
 
@@ -27,6 +31,9 @@ public class SessionData {
     private static final SimpleObjectProperty<Trainer> trainer = new SimpleObjectProperty<>();
     private static final SimpleStringProperty comment = new SimpleStringProperty();
 
+    /**
+     * Cancels the session by resetting all the session data.
+     */
     public static void cancel() {
         courtReservationScene = null;
         summaryScene = null;
@@ -48,19 +55,24 @@ public class SessionData {
         trainer.set(null);
     }
 
+    /**
+     * Calculates the total price of the session based on the court, racket, trainer, and duration.
+     *
+     * @return The total price of the session.
+     */
     public static BigDecimal getTotalPrice() {
-        if (reservationStart.getValue() == null ||
-                reservationDuration.getValue() == null) return BigDecimal.ZERO;
+        if (reservationStart.getValue() == null || reservationDuration.getValue() == null) {
+            return BigDecimal.ZERO;
+        }
 
         BigDecimal totalPrice = BigDecimal.ZERO;
         BigDecimal duration = BigDecimal.valueOf(reservationDuration.getValue().toHours());
 
-        if (court.getValue() != null) {
-            if (court.getValue() instanceof CourtRoofed)
-                totalPrice = totalPrice.add(CourtRoofed.getPricePerHour().multiply(duration))
-                                       .add(CourtRoofed.getHeatingSurcharge(reservationStart.getValue().toLocalDate()));
-            else if (court.getValue() instanceof CourtUnroofed)
-                totalPrice = totalPrice.add(CourtUnroofed.getPricePerHour().multiply(duration));
+        if (court.getValue() instanceof CourtRoofed) {
+            totalPrice = totalPrice.add(CourtRoofed.getPricePerHour().multiply(duration))
+                    .add(CourtRoofed.getHeatingSurcharge(reservationStart.getValue().toLocalDate()));
+        } else if (court.getValue() instanceof CourtUnroofed) {
+            totalPrice = totalPrice.add(CourtUnroofed.getPricePerHour().multiply(duration));
         }
 
         if (racket.getValue() != null) {
@@ -74,35 +86,76 @@ public class SessionData {
         return totalPrice;
     }
 
+    /**
+     * Returns the court property.
+     *
+     * @return The court property.
+     */
     public static SimpleObjectProperty<Court> courtProperty() {
         return court;
     }
 
+    /**
+     * Returns the reservation start property.
+     *
+     * @return The reservation start property.
+     */
     public static SimpleObjectProperty<LocalDateTime> reservationStartProperty() {
         return reservationStart;
     }
 
+    /**
+     * Returns the reservation duration property.
+     *
+     * @return The reservation duration property.
+     */
     public static SimpleObjectProperty<Duration> reservationDurationProperty() {
         return reservationDuration;
     }
 
+    /**
+     * Returns the participant property.
+     *
+     * @return The participant property.
+     */
     public static SimpleObjectProperty<Person> participantProperty() {
         return participant;
     }
 
+    /**
+     * Returns the client property.
+     *
+     * @return The client property.
+     */
     public static SimpleObjectProperty<Person> clientProperty() {
         return client;
     }
 
+    /**
+     * Returns the racket property.
+     *
+     * @return The racket property.
+     */
     public static SimpleObjectProperty<Racket> racketProperty() {
         return racket;
     }
 
+    /**
+     * Returns the trainer property.
+     *
+     * @return The trainer property.
+     */
     public static SimpleObjectProperty<Trainer> trainerProperty() {
         return trainer;
     }
 
+    /**
+     * Returns the comment property.
+     *
+     * @return The comment property.
+     */
     public static SimpleStringProperty commentProperty() {
         return comment;
     }
 }
+
