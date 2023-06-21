5d17d40e0191c02c41f7ceff20cc062f66da8712
magladko <kornel.marek@gmail.com>
2023-06-21 21:49:32 +0200

    CourtReservationApp docs

--- src/main/java/mas/CourtReservationApp.java
+++ src/main/java/mas/CourtReservationApp.java
@@ -7,29 +7,43 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import lombok.Getter;
-import mas.entity.StaticStorage;
 import mas.util.DBController;
 
 import java.util.Objects;
 
+/**
+ * Main class for the Tennis Courts Management Application.
+ * This class extends the JavaFX Application class and serves as the entry point for the application.
+ */
 public class CourtReservationApp extends Application {
 
-    @Getter
+    /**
+     * The primary stage of the application.
+     */
     private static Stage stage;
 
-    @Getter
+    /**
+     * An additional stage used for displaying additional windows in the application.
+     */
     private static Stage additionalStage;
 
+    /**
+     * Retrieves the primary stage of the application.
+     *
+     * @return The primary stage
+     */
+    public static Stage getStage() {
+        return stage;
+    }
+
+    /**
+     * Entry point for the JavaFX application.
+     *
+     * @param stage The primary stage
+     * @throws Exception if an error occurs during application startup
+     */
     @Override
     public void start(Stage stage) throws Exception {
-        DBController db = DBController.INSTANCE;
-
-//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("court-reservation.fxml")));
-//        stage.setTitle("Court Reservation App");
-//        stage.setMinWidth(500);
-//        stage.setMinHeight(400);
-//
         CourtReservationApp.stage = stage;
         CourtReservationApp.additionalStage = new Stage();
         CourtReservationApp.additionalStage.setOnCloseRequest(e -> {
@@ -41,21 +55,23 @@ public class CourtReservationApp extends Application {
         CourtReservationApp.stage.setScene(new Scene(root, 600, 400));
         CourtReservationApp.stage.setMinWidth(200);
         CourtReservationApp.stage.setMinHeight(200);
-        CourtReservationApp.stage.setTitle("Court reservation app");
+        CourtReservationApp.stage.setTitle("Court Reservation App");
         CourtReservationApp.stage.show();
-
     }
 
+    /**
+     * Main method for launching the application.
+     *
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         DBController.INSTANCE.setEm(entityManagerFactory.createEntityManager());
         try {
-
-//            DBController.INSTANCE.seedDb();
+            // DBController.INSTANCE.seedDb();
             launch(args);
-
         } finally {
-            if(DBController.INSTANCE.getEm().getTransaction().isActive()) {
+            if (DBController.INSTANCE.getEm().getTransaction().isActive()) {
                 DBController.INSTANCE.getEm().getTransaction().rollback();
             }
             DBController.INSTANCE.getEm().close();
