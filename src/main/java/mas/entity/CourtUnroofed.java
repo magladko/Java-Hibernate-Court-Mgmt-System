00560b967f82fbfa060f2ef435ce097388d7e46b
magladko <kornel.marek@gmail.com>
2023-06-21 19:13:44 +0200

    CourtUnroofed docs

--- src/main/java/mas/entity/CourtUnroofed.java
+++ src/main/java/mas/entity/CourtUnroofed.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 import mas.util.DBController;
 
 import java.math.BigDecimal;
-import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -16,16 +15,33 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
+/**
+ * Represents an unroofed court in a tennis court management application.
+ * It extends the base Court class and adds specific properties related to unroofed courts.
+ */
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class CourtUnroofed extends Court {
 
+    /**
+     * Constructs a CourtUnroofed with the given number and surface type.
+     * @param number the court number
+     * @param surfaceType the surface type of the court
+     */
     public CourtUnroofed(Integer number, SurfaceType surfaceType) {
         super(number, surfaceType);
     }
 
+    /**
+     * Checks if the court is available for booking during the specified time period.
+     * The court is available if it falls within the season start and end dates,
+     * and if it is not already booked for the specified time period.
+     * @param from the start time of the booking
+     * @param duration the duration of the booking
+     * @return true if the court is available, false otherwise
+     */
     @Override
     public boolean isAvailable(LocalDateTime from, Duration duration) {
         return from.isAfter(getSeasonStart().atStartOfDay()) &&
@@ -33,6 +49,10 @@ public class CourtUnroofed extends Court {
                 super.isAvailable(from, duration);
     }
 
+    /**
+     * Returns the string representation of the CourtUnroofed.
+     * @return the string representation of the CourtUnroofed
+     */
     @Override
     public String toString() {
         return "CourtUnroofed{" +
@@ -41,21 +61,37 @@ public class CourtUnroofed extends Court {
                 '}';
     }
 
+    /**
+     * Returns the price per hour for an unroofed court from the database.
+     * @return the price per hour for an unroofed court
+     */
     public static BigDecimal getPricePerHour() {
         return DBController.INSTANCE.getEm()
                 .createQuery("select ss.courtUnroofedPricePerHour from StaticStorage ss", BigDecimal.class).getSingleResult();
     }
 
+    /**
+     * Returns the start date of the season for an unroofed court from the database.
+     * @return the start date of the season for an unroofed court
+     */
     public static LocalDate getSeasonStart() {
         return DBController.INSTANCE.getEm()
                 .createQuery("select ss.courtUnroofedSeasonStart from StaticStorage ss", LocalDate.class).getSingleResult();
     }
 
+    /**
+     * Returns the end date of the season for an unroofed court from the database.
+     * @return the end date of the season for an unroofed court
+     */
     public static LocalDate getSeasonEnd() {
         return DBController.INSTANCE.getEm()
                 .createQuery("select ss.courtUnroofedSeasonEnd from StaticStorage ss", LocalDate.class).getSingleResult();
     }
 
+    /**
+     * Returns the information text for the unroofed court.
+     * @return the information text for the unroofed court
+     */
     public String getInfoTxt() {
         DateTimeFormatter df = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.getDefault());
 
