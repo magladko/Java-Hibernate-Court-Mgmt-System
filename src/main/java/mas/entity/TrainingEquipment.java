44144bcb8e88756da7a9c29209f49b5bfe166e96
magladko <kornel.marek@gmail.com>
2023-06-21 21:02:28 +0200

    TrainingEquipment docs

--- src/main/java/mas/entity/TrainingEquipment.java
+++ src/main/java/mas/entity/TrainingEquipment.java
@@ -7,6 +7,10 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+/**
+ * Represents a training equipment used in a tennis court management application.
+ * It extends the Equipment class.
+ */
 @Entity
 @Getter
 @Setter
@@ -16,17 +20,35 @@ public class TrainingEquipment extends Equipment {
     private String name;
     private String description;
 
+    /**
+     * Constructs a TrainingEquipment object with the given name and description.
+     *
+     * @param name        the name of the training equipment
+     * @param description the description of the training equipment
+     */
     public TrainingEquipment(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Constructs a TrainingEquipment object with the given notes, name, and description.
+     *
+     * @param notes       the notes for the training equipment
+     * @param name        the name of the training equipment
+     * @param description the description of the training equipment
+     */
     public TrainingEquipment(String notes, String name, String description) {
         super(notes);
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Returns a string representation of the TrainingEquipment object.
+     *
+     * @return a string representation of the TrainingEquipment object
+     */
     @Override
     public String toString() {
         return "TrainingEquipment{" +
@@ -36,6 +58,12 @@ public class TrainingEquipment extends Equipment {
                 '}';
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param o the reference object with which to compare
+     * @return true if this object is the same as the o argument; false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -46,6 +74,11 @@ public class TrainingEquipment extends Equipment {
         return getDescription() != null ? getDescription().equals(that.getDescription()) : that.getDescription() == null;
     }
 
+    /**
+     * Returns a hash code value for the object.
+     *
+     * @return a hash code value for this object
+     */
     @Override
     public int hashCode() {
         int result = super.hashCode();
