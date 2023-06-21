d6047d528bf3b57516c20f7e6cd2dfb744407ce4
magladko <kornel.marek@gmail.com>
2023-06-21 21:25:56 +0200

    MoneyFormatCell docs

--- src/main/java/mas/util/MoneyFormatCell.java
+++ src/main/java/mas/util/MoneyFormatCell.java
@@ -1,12 +1,24 @@
 package mas.util;
 
 import javafx.scene.control.TableCell;
-import mas.entity.Court;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 
-public class MoneyFormatCell extends TableCell<Court, BigDecimal> {
+/**
+ * This class is a custom TableCell for displaying BigDecimal values as formatted currency in a TableView.
+ * It is used in the Tennis Courts management application to format money values in the UI.
+ *
+ * @param <Court> the type of the TableView row items
+ */
+public class MoneyFormatCell<Court> extends TableCell<Court, BigDecimal> {
+
+    /**
+     * Updates the item value of the TableCell and displays it as formatted currency.
+     *
+     * @param item  the BigDecimal item to display
+     * @param empty a boolean indicating if the cell is empty or not
+     */
     @Override
     protected void updateItem(BigDecimal item, boolean empty) {
         super.updateItem(item, empty);
