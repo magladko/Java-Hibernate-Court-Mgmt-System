df2d08c1ee8189c36fefa2516bf044ac51c18652
magladko <kornel.marek@gmail.com>
2023-06-21 21:45:03 +0200

    WeakAdapter docs

--- src/main/java/mas/util/WeakAdapter.java
+++ src/main/java/mas/util/WeakAdapter.java
@@ -15,54 +15,98 @@ import javafx.collections.WeakListChangeListener;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents a weak adapter for managing change listeners in a Tennis Courts management application.
+ * It allows adding and removing various types of change listeners, such as ChangeListener, InvalidationListener,
+ * ListChangeListener, and binding StringProperty and BooleanProperty to corresponding expressions.
+ */
 public class WeakAdapter {
 
-    ArrayList<Object> listenerRefs = new ArrayList<>();
+    private final ArrayList<Object> listenerRefs = new ArrayList<>();
 
+    /**
+     * Constructs a new WeakAdapter.
+     */
     public WeakAdapter() {}
 
+    /**
+     * Clears all the registered listeners.
+     */
     public void dispose() {
         listenerRefs.clear();
     }
 
+    /**
+     * Removes the specified ChangeListener from the registered listeners.
+     *
+     * @param listener the ChangeListener to remove
+     * @param <T> the type of the value being observed
+     */
     public final <T> void remove(ChangeListener<T> listener) {
         listenerRefs.remove(listener);
     }
 
-    public final <T> void addChangeListener(final ObservableValue observable, ChangeListener<T> listener) {
+    /**
+     * Adds a ChangeListener to the given ObservableValue with weak referencing.
+     * The listener will be automatically removed when it becomes garbage collected.
+     *
+     * @param observable the ObservableValue to listen to
+     * @param listener the ChangeListener to add
+     * @param <T> the type of the value being observed
+     */
+    public final <T> void addChangeListener(final ObservableValue<T> observable, ChangeListener<T> listener) {
         listenerRefs.add(listener);
         observable.addListener(new WeakChangeListener<>(listener));
     }
 
+    /**
+     * Adds a ListChangeListener with weak referencing.
+     * The listener will be automatically removed when it becomes garbage collected.
+     *
+     * @param listener the ListChangeListener to add
+     * @param <T> the type of the elements in the list
+     * @return the created WeakListChangeListener
+     */
     public final <T> WeakListChangeListener<T> addListChangeListener(ListChangeListener<T> listener) {
         listenerRefs.add(listener);
         return new WeakListChangeListener<>(listener);
     }
 
-    public void addInvalidationListener(final Observable listened, InvalidationListener listener) {
+    /**
+     * Adds an InvalidationListener to the given Observable with weak referencing.
+     * The listener will be automatically removed when it becomes garbage collected.
+     *
+     * @param observed the Observable to listen to
+     * @param listener the InvalidationListener to add
+     */
+    public void addInvalidationListener(final Observable observed, InvalidationListener listener) {
         listenerRefs.add(listener);
-        listened.addListener(new WeakInvalidationListener(listener));
+        observed.addListener(new WeakInvalidationListener(listener));
     }
 
+    /**
+     * Binds a StringProperty to a StringExpression with weak referencing.
+     * The StringProperty will be automatically updated when the StringExpression changes.
+     *
+     * @param propertyToUpdate the StringProperty to update
+     * @param expressionToListen the StringExpression to listen to
+     */
     public final void stringBind(final StringProperty propertyToUpdate, final StringExpression expressionToListen) {
-        ChangeListener<String> listener = new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> ov, String t, String name) {
-                propertyToUpdate.set(name);
-            }
-        };
+        ChangeListener<String> listener = (ov, oldValue, newValue) -> propertyToUpdate.set(newValue);
         listenerRefs.add(listener);
         expressionToListen.addListener(new WeakChangeListener<>(listener));
         listener.changed(null, null, expressionToListen.get());
     }
 
+    /**
+     * Binds a BooleanProperty to a BooleanExpression with weak referencing.
+     * The BooleanProperty will be automatically updated when the BooleanExpression changes.
+     *
+     * @param propertyToUpdate the BooleanProperty to update
+     * @param expressionToListen the BooleanExpression to listen to
+     */
     public final void booleanBind(final BooleanProperty propertyToUpdate, final BooleanExpression expressionToListen) {
-        ChangeListener<Boolean> listener = new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean name) {
-                propertyToUpdate.set(name);
-            }
-        };
+        ChangeListener<Boolean> listener = (ov, oldValue, newValue) -> propertyToUpdate.set(newValue);
         listenerRefs.add(listener);
         expressionToListen.addListener(new WeakChangeListener<>(listener));
         propertyToUpdate.set(expressionToListen.get());
