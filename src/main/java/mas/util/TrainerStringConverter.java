c071334c2b76f652b5f83149ce142914fee12e8e
magladko <kornel.marek@gmail.com>
2023-06-21 21:36:22 +0200

    TrainerStringConverter docs

--- src/main/java/mas/util/TrainerStringConverter.java
+++ src/main/java/mas/util/TrainerStringConverter.java
@@ -5,8 +5,19 @@ import mas.entity.Trainer;
 
 import java.text.NumberFormat;
 
+/**
+ * A converter class that converts Trainer objects to Strings and vice versa.
+ * The converted String representation includes the trainer's name, surname, qualification, and price per hour.
+ * This class is used in the Tennis Courts management application for display and input purposes.
+ */
 public class TrainerStringConverter extends StringConverter<Trainer> {
 
+    /**
+     * Converts a Trainer object to its String representation.
+     *
+     * @param trainer the Trainer object to be converted
+     * @return the String representation of the Trainer object
+     */
     @Override
     public String toString(Trainer trainer) {
         if (trainer == null) {
@@ -20,6 +31,12 @@ public class TrainerStringConverter extends StringConverter<Trainer> {
         }
     }
 
+    /**
+     * Converts a String to a Trainer object.
+     *
+     * @param string the String to be converted
+     * @return the Trainer object represented by the String
+     */
     @Override
     public Trainer fromString(String string) {
         // Implement if needed
