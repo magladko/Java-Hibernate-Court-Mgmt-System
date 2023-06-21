a806ab0c69c3db5e1393cf24b4d457a0476d7098
magladko <kornel.marek@gmail.com>
2023-06-21 21:28:20 +0200

    ParticipantComboBoxStringConverter docs

--- src/main/java/mas/util/ParticipantComboBoxStringConverter.java
+++ src/main/java/mas/util/ParticipantComboBoxStringConverter.java
@@ -4,11 +4,24 @@ import javafx.util.StringConverter;
 import mas.entity.Person;
 import org.hibernate.TypeMismatchException;
 
+/**
+ * A custom StringConverter implementation for converting Person objects to strings and vice versa.
+ */
 public class ParticipantComboBoxStringConverter extends StringConverter<Person> {
 
+    /**
+     * Converts a Person object to its string representation.
+     *
+     * @param person the Person object to convert
+     * @return the string representation of the Person object
+     * @throws TypeMismatchException if the Person object is not a participant nor a client
+     */
     @Override
     public String toString(Person person) {
-        if (person == null) return "";
+        if (person == null) {
+            return "";
+        }
+
         if (person.getPersonTypes().contains(Person.PersonType.CLIENT)) {
             return "Klient: " + person.getName() + " " + person.getSurname();
         } else if (person.getPersonTypes().contains(Person.PersonType.PARTICIPANT)) {
@@ -18,10 +31,17 @@ public class ParticipantComboBoxStringConverter extends StringConverter<Person> 
             }
             return result;
         } else {
-            throw new TypeMismatchException("Person is not a participant nor client");
+            throw new TypeMismatchException("Person is not a participant nor a client");
         }
     }
 
+    /**
+     * Converts a string to a Person object (not supported).
+     *
+     * @param string the string to convert
+     * @return the converted Person object
+     * @throws UnsupportedOperationException because this operation is not supported
+     */
     @Override
     public Person fromString(String string) {
         throw new UnsupportedOperationException("Not supported yet.");
