ea73160646bfc81625d344574cbf339d61de90bc
magladko <kornel.marek@gmail.com>
2023-06-21 20:48:46 +0200

    StaticStorage docs

--- src/main/java/mas/entity/StaticStorage.java
+++ src/main/java/mas/entity/StaticStorage.java
@@ -9,6 +9,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * Entity class for persisting static values related to the Tennis Courts management application using Hibernate mechanisms.
+ */
 @Entity
 @Getter
 @Setter
@@ -19,25 +22,57 @@ public class StaticStorage {
     @Column(nullable = false)
     private Long id;
 
+    /**
+     * The opening hour of the tennis court.
+     */
     @Column(unique = true)
     private LocalTime courtOpeningHour;
+
+    /**
+     * The closing hour of the tennis court.
+     */
     @Column(unique = true)
     private LocalTime courtClosingHour;
 
+    /**
+     * The price per hour for a roofed tennis court.
+     */
     @Column(unique = true)
     private BigDecimal courtRoofedPricePerHour;
+
+    /**
+     * The surcharge amount for heating a roofed tennis court during the heating season.
+     */
     @Column(unique = true)
     private BigDecimal courtRoofedHeatingSurcharge;
+
+    /**
+     * The start date of the heating season for a roofed tennis court.
+     */
     @Column(unique = true)
     private LocalDate courtRoofedHeatingSeasonStart;
+
+    /**
+     * The end date of the heating season for a roofed tennis court.
+     */
     @Column(unique = true)
     private LocalDate courtRoofedHeatingSeasonEnd;
 
+    /**
+     * The price per hour for an unroofed tennis court.
+     */
     @Column(unique = true)
     private BigDecimal courtUnroofedPricePerHour;
+
+    /**
+     * The start date of the season for an unroofed tennis court.
+     */
     @Column(unique = true)
     private LocalDate courtUnroofedSeasonStart;
+
+    /**
+     * The end date of the season for an unroofed tennis court.
+     */
     @Column(unique = true)
     private LocalDate courtUnroofedSeasonEnd;
-
 }
